66287707d35df4f34ecc8e109fb8cf197d2af164
Amandeep Kochhar <amandeep.netweb@gmail.com>
2025-04-14 20:59:14 +0530

    added twitter handler

--- src/utils/handlers/index.ts
+++ src/utils/handlers/index.ts
@@ -1,5 +1,10 @@
 import { AppScheme } from "@/types/link-handlers";
 import { youtubeHandler } from "./youtubeHandler";
 import { instagramHandler } from "./instagramHandler";
+import { twitterHandler } from "./twitterHandler";
 
-export const handlers: AppScheme[] = [youtubeHandler, instagramHandler];
+export const handlers: AppScheme[] = [
+  youtubeHandler,
+  instagramHandler,
+  twitterHandler,
+];
---
+++ src/utils/handlers/twitterHandler.ts
@@ -0,0 +1,22 @@
+import { AppScheme } from "@/types/link-handlers";
+
+export const twitterHandler: AppScheme = {
+  name: "Twitter",
+  domains: ["twitter.com", "www.twitter.com", "x.com", "www.x.com"],
+  patterns: [
+    {
+      // 🎯 Tweet detail: /username/status/1234567890
+      regex: /^\/[^/]+\/status\/(\d+)/,
+      iosScheme: (match) => `twitter://status?id=${match[1]}`,
+      androidScheme: (match) =>
+        `intent://status?id=${match[1]}#Intent;package=com.twitter.android;scheme=twitter;end`,
+    },
+    {
+      // 🎯 Profile: /@username or /username
+      regex: /^\/@?([^/]+)$/,
+      iosScheme: (match) => `twitter://user?screen_name=${match[1]}`,
+      androidScheme: (match) =>
+        `intent://user?screen_name=${match[1]}#Intent;package=com.twitter.android;scheme=twitter;end`,
+    },
+  ],
+};
