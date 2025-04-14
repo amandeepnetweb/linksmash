0576597e7b765be06e4e521ef83eeb134f6b1fb1
Amandeep Kochhar <amandeep.netweb@gmail.com>
2025-04-14 22:20:49 +0530

    updates

--- src/utils/handlers/twitterHandler.ts
+++ src/utils/handlers/twitterHandler.ts
@@ -23,7 +23,7 @@ export const twitterHandler: AppScheme = {
       regex: /^\/i\/communities\/(\d+)/,
       iosScheme: (match) => `https://x.com/i/communities/${match[1]}`,
       androidScheme: (match) =>
-        `intent://x.com/i/communities/${match[1]}#Intent;package=com.twitter.android;scheme=twitter;end`,
+        `intent://i/communities/${match[1]}#Intent;scheme=twitter;package=com.twitter.android;end`,
     },
   ],
 };
