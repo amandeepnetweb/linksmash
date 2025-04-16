9a9b8e76dddb9a1891ab5f69794105226c62a2a5
Amandeep Kochhar <amandeep.netweb@gmail.com>
2025-04-16 10:54:41 +0530

    updated linkedin handler

--- src/utils/handlers/linkedinHandler.ts
+++ src/utils/handlers/linkedinHandler.ts
@@ -19,5 +19,19 @@ export const linkedinHandler: AppScheme = {
       androidScheme: (match) =>
         `intent://linkedin.com/feed/update/urn:li:activity:${match[1]}#Intent;package=com.linkedin.android;scheme=https;end`,
     },
+    {
+      // 🏢 Company Profile (/company/:slug)
+      regex: /^\/company\/([^/]+)\/?$/,
+      iosScheme: (match) => `https://www.linkedin.com/company/${match[1]}`,
+      androidScheme: (match) =>
+        `intent://linkedin.com/company/${match[1]}#Intent;package=com.linkedin.android;scheme=https;end`,
+    },
+    {
+      // 💼 Job Post (/jobs/view/:jobId)
+      regex: /^\/jobs\/view\/(\d+)\/?$/,
+      iosScheme: (match) => `https://www.linkedin.com/jobs/view/${match[1]}`,
+      androidScheme: (match) =>
+        `intent://linkedin.com/jobs/view/${match[1]}#Intent;package=com.linkedin.android;scheme=https;end`,
+    },
   ],
 };
